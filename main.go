404112bcf30f872d9bca00c85590e17f80eec995
Christian Persson <Saser@users.noreply.github.com>
2019-10-20 22:23:13 +0200

    main: add code to set up connection to X server
    This will serve as the starting point to all X server communication,
    as well as handling events in the event loop.

--- main.go
+++ main.go
@@ -5,6 +5,8 @@ import (
 	"log"
 	"os"
 	"os/signal"
+
+	"github.com/BurntSushi/xgbutil"
 )
 
 func withCancelOnInterrupt(ctx context.Context) (context.Context, context.CancelFunc) {
@@ -20,6 +22,15 @@ func withCancelOnInterrupt(ctx context.Context) (context.Context, context.Cancel
 
 func main() {
 	logger := log.New(os.Stderr, "", log.Ldate|log.Ltime|log.Lshortfile)
+
+	// Set up a connection to the X server.
+	logger.Println("setting up connection to X server")
+	_, err := xgbutil.NewConn()
+	if err != nil {
+		logger.Fatalf("setting up connection to X server failed: %+v", err)
+	}
+	logger.Println("set up connection to X server")
+
 	ctx, cancel := withCancelOnInterrupt(context.Background())
 	defer cancel()
 	logger.Println("waiting for interrupt")
