bccc236560903fd93b13cbd798f75191eebed5d2
Christian Persson <Saser@users.noreply.github.com>
2019-10-21 14:06:31 +0200

    main,switcher: channel focus events from `Watcher` to `Switcher`
    As of right now, the switcher does not do anything, but now it is
    aware when a window is focused at least.

--- switcher.go
+++ switcher.go
@@ -1,16 +1,27 @@
 package main
 
-import "log"
+import (
+	"log"
+
+	"github.com/BurntSushi/xgb/xproto"
+)
 
 type Switcher struct {
 	logger *log.Logger
 
+	focused <-chan xproto.Window
+
 	done chan struct{}
 }
 
-func NewSwitcher(logger *log.Logger) *Switcher {
+func NewSwitcher(
+	logger *log.Logger,
+	focused <-chan xproto.Window,
+) *Switcher {
 	return &Switcher{
 		logger: logger,
+
+		focused: focused,
 
 		done: make(chan struct{}, 1),
 	}
@@ -18,8 +29,16 @@ func NewSwitcher(logger *log.Logger) *Switcher {
 
 func (s *Switcher) Start() error {
 	s.logger.Println("starting switcher")
-	<-s.done
-	s.logger.Println("received done signal")
+loop:
+	for {
+		select {
+		case window := <-s.focused:
+			s.logger.Printf("received focus on window %d", window)
+		case <-s.done:
+			s.logger.Println("received done signal")
+			break loop
+		}
+	}
 	s.logger.Println("stopped switcher")
 	return nil
 }
