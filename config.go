5ec532cc990d72f3f0492073b3f57bf798f35910
Christian Persson <Saser@users.noreply.github.com>
2019-10-24 23:49:32 +0200

    config: add simple `Config` type with validation and a few tests

--- config.go
+++ config.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"errors"
+	"fmt"
 )
 
 type Spec struct {
@@ -11,6 +12,17 @@ type Spec struct {
 func (s *Spec) Validate() error {
 	if s.Layout == "" {
 		return errors.New("validate spec: layout is empty")
+	}
+	return nil
+}
+
+type Config struct {
+	Default Spec
+}
+
+func (c *Config) Validate() error {
+	if err := c.Default.Validate(); err != nil {
+		return fmt.Errorf("validate config: %w", err)
 	}
 	return nil
 }
